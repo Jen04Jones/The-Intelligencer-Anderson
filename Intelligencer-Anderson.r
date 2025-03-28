91f6c595cc5f127842dfed1d288b2ba4e9afb29b
Jen M. Jones <jjone67@clemson.edu>
2025-03-28 00:45:38 -0400

    Update Intelligencer-Anderson.r
    Added corrected output path.

--- Intelligencer-Anderson.r
+++ Intelligencer-Anderson.r
@@ -11,7 +11,7 @@ lccn <- "sn84026965"  # The Anderson Intelligencer's LCCN
 keyword <- "textile"  # Search keyword
 
 # Output path
-output_path <- "github.com/Jen04Jones/Anderson-Mills/anderson_articles.csv"
+output_path <- "github.com/Jen04Jones/The-Intelligencer-Anderson/anderson_articles.csv"
 
 write.csv(results %>% select(title, date, url, cleaned_text),
           output_path, row.names = FALSE)
